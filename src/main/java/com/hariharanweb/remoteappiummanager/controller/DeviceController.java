89ce00c4978b2b36bc330af9d0e678a71544d020
ttllonsaikrisv <tllonsaikrisv@thetrainline.com>
2018-01-16 14:17:11 +0530

    #fix 17 end to get respecting udid

--- src/main/java/com/hariharanweb/remoteappiummanager/controller/DeviceController.java
+++ src/main/java/com/hariharanweb/remoteappiummanager/controller/DeviceController.java
@@ -1,14 +1,23 @@
 package com.hariharanweb.remoteappiummanager.controller;
 
+import com.thoughtworks.android.AndroidManager;
 import com.thoughtworks.device.DeviceManager;
+import com.thoughtworks.device.SimulatorManager;
+import com.thoughtworks.iOS.IOSManager;
 import spark.Route;
 
 public class DeviceController {
 
     private DeviceManager deviceManager;
+    private SimulatorManager simulatorManager;
+    private IOSManager iosManager;
+    private AndroidManager androidManager;
 
     public DeviceController(){
         deviceManager = new DeviceManager();
+        simulatorManager = new SimulatorManager();
+        iosManager = new IOSManager();
+        androidManager = new AndroidManager();
     }
 
     public Route getDevices = (request, response) -> {
@@ -23,6 +32,36 @@ public class DeviceController {
     public Route getDevice = (request, response) -> {
         try{
             return deviceManager.getDevice(request.params(":udid"));
+        }catch (Exception e){
+            response.status(404);
+            response.body(e.getMessage());
+        }
+        return null;
+    };
+
+    public Route getSimulators = (request, response) -> {
+        try{
+            return simulatorManager.getAllSimulators("iOS");
+        }catch (Exception e){
+            response.status(404);
+            response.body(e.getMessage());
+        }
+        return null;
+    };
+
+    public Route getIOSDevices = (request, response) -> {
+        try{
+            return iosManager.getDevices();
+        }catch (Exception e){
+            response.status(404);
+            response.body(e.getMessage());
+        }
+        return null;
+    };
+
+    public Route getAndroidDevices = (request, response) -> {
+        try{
+            return androidManager.getDevices();
         }catch (Exception e){
             response.status(404);
             response.body(e.getMessage());
